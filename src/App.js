9755d321ae1ac06ddf1bbfe3d72ada21d59591bf
Bruno <brunohgrau99@gmail.com>
2024-04-08 17:03:46 -0300

    tailwind

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold  underline">Hello world!</h1>
-      <h1 className="text-xl bg-slate-600"> Typescale </h1>
+      <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">
+        Save changes
+      </button>
     </div>
   );
 }
